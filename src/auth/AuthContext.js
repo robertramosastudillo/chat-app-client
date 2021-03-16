999227c83cb699e78e5f06613b79b6c31292153d
robertramosastudillo <robertramosastudillo@gmail.com>
2021-03-16 10:56:52 -0500

    Se implementaron las proteccion de rutas

---
+++ src/auth/AuthContext.js
@@ -0,0 +1,105 @@
+import React, { createContext, useCallback, useState } from "react";
+import { fetchConToken, fetchSinToken } from "../helpers/fetch";
+
+export const AuthContext = createContext();
+
+const initialState = {
+  uid: null,
+  checking: true,
+  logged: false,
+  name: null,
+  email: null,
+};
+
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState(initialState);
+
+  const login = async (email, password) => {
+    const resp = await fetchSinToken("login", { email, password }, "POST");
+
+    if (resp.ok) {
+      localStorage.setItem("token", resp.token);
+      const { usuario } = resp;
+      setAuth({
+        uid: usuario.uid,
+        checking: false,
+        logged: true,
+        name: usuario.nombre,
+        email: usuario.email,
+      });
+      console.log("Autenticado");
+    }
+    return resp.ok;
+  };
+
+  const register = async (nombre, email, password) => {
+    const resp = await fetchSinToken(
+      "login/new",
+      { nombre, email, password },
+      "POST"
+    );
+
+    if (resp.ok) {
+      localStorage.setItem("token", resp.token);
+      const { usuario } = resp;
+      setAuth({
+        uid: usuario.uid,
+        checking: false,
+        logged: true,
+        name: usuario.nombre,
+        email: usuario.email,
+      });
+      console.log("Autenticado");
+      return true;
+    }
+    return resp.msg;
+  };
+  const verificaToken = useCallback(async () => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      setAuth({
+        uid: null,
+        checking: false,
+        logged: false,
+        name: null,
+        email: null,
+      });
+
+      return false;
+    }
+
+    const resp = await fetchConToken("login/renew");
+    if (resp.ok) {
+      localStorage.setItem("token", resp.token);
+      const { usuario } = resp;
+      setAuth({
+        uid: usuario.uid,
+        checking: false,
+        logged: true,
+        name: usuario.nombre,
+        email: usuario.email,
+      });
+      console.log("Autenticado");
+      return true;
+    } else {
+      setAuth({
+        uid: null,
+        checking: false,
+        logged: false,
+        name: null,
+        email: null,
+      });
+
+      return false;
+    }
+  }, []);
+  const logout = () => {};
+
+  return (
+    <AuthContext.Provider
+      value={{ auth, login, register, verificaToken, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,66 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import { AuthContext } from "../auth/AuthContext";
 
 export const LoginPage = () => {
+  const { login } = useContext(AuthContext);
+
+  const [form, setForm] = useState({
+    email: "test@test.com",
+    password: "12345",
+    rememberme: false,
+  });
+
+  useEffect(() => {
+    const email = localStorage.getItem("email");
+    if (email) {
+      setForm((form) => ({
+        ...form,
+        email,
+        rememberme: true,
+      }));
+    }
+  }, []);
+
+  const onChange = ({ target }) => {
+    const { name, value } = target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const toggleCheck = () => {
+    setForm({
+      ...form,
+      rememberme: !form.rememberme,
+    });
+  };
+
+  const onSubmit = async (ev) => {
+    ev.preventDefault();
+
+    form.rememberme
+      ? localStorage.setItem("email", form.email)
+      : localStorage.removeItem("email");
+
+    const { email, password } = form;
+    const ok = await login(email, password);
+    if (!ok) {
+      Swal.fire("Error", "Verifique el usuario y contraseña", "error");
+    }
+  };
+
+  const todoOk = () => {
+    return form.email.length > 0 && form.password.length > 0 ? true : false;
+  };
+
   return (
-    <form className="login100-form validate-form flex-sb flex-w">
+    <form
+      className="login100-form validate-form flex-sb flex-w"
+      onSubmit={onSubmit}
+    >
       <span className="login100-form-title mb-3">Chat - Ingreso</span>
 
       <div className="wrap-input100 validate-input mb-3">
@@ -12,6 +69,8 @@ export const LoginPage = () => {
           type="email"
           name="email"
           placeholder="Email"
+          value={form.email}
+          onChange={onChange}
         />
         <span className="focus-input100"></span>
       </div>
@@ -22,17 +81,21 @@ export const LoginPage = () => {
           type="password"
           name="password"
           placeholder="Password"
+          value={form.password}
+          onChange={onChange}
         />
         <span className="focus-input100"></span>
       </div>
 
       <div className="row mb-3">
-        <div className="col">
+        <div className="col" onClick={() => toggleCheck()}>
           <input
             className="input-checkbox100"
             id="ckb1"
             type="checkbox"
-            name="remember-me"
+            name="rememberme"
+            checked={form.rememberme}
+            readOnly
           />
           <label className="label-checkbox100">Recordarme</label>
         </div>
@@ -45,7 +108,13 @@ export const LoginPage = () => {
       </div>
 
       <div className="container-login100-form-btn m-t-17">
-        <button className="login100-form-btn">Ingresar</button>
+        <button
+          type="submit"
+          disabled={!todoOk()}
+          className="login100-form-btn"
+        >
+          Ingresar
+        </button>
       </div>
     </form>
   );
